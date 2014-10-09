a86bdd89fa210af65f416c61b4b0bbb3e16133f7
Nassim A. <bashou@users.noreply.github.com>
2014-10-09 15:36:00 +0200

    Update slave.pp

--- manifests/slave.pp
+++ manifests/slave.pp
@@ -40,7 +40,7 @@ define smokeping::slave(
       }
     }
   
-  @@concat::fragment { "${::hostname}-secret_${slave_name}":
+  concat::fragment { "${::hostname}-secret_${slave_name}":
       target  => $smokeping::slave_secrets,
       order   => 10,
       content => "${slave_name}:${random_value}\n",
@@ -48,12 +48,12 @@ define smokeping::slave(
   }
 
   $filename = "${smokeping::slave_dir}/${display_name}"
-  @@file { $filename:
+  file { $filename:
       content => template('smokeping/slave.erb'),
       tag     => "smokeping-slave-${master}",
   }
 
-  @@concat::fragment { $name:
+  concat::fragment { $name:
       order   => 20,
       target  => '/etc/smokeping/config.d/Slaves',
       content => "@include ${filename}\n",
